caeb038ecc98aebf8a28955f9f9e27c0b911a22f
Rolem Galvez <rolemgalvez@gmail.com>
2023-01-24 11:56:00 +0800

    Add project files.

---
+++ JobPositions/Program.cs
@@ -0,0 +1,19 @@
+﻿//Welcome message
+Console.WriteLine("Job Positions");
+Console.WriteLine("-------------");
+Console.WriteLine();
+
+//Prepare values
+string data = "Functional Consultant,Technical Support,Manager,Administrator";
+List<string> positions = data.Split(',').ToList();
+
+//Show values
+foreach (string position in positions)
+{
+    Console.WriteLine(position);
+}
+
+//End
+Console.WriteLine();
+Console.WriteLine("End of program.");
+Console.ReadLine();
---
+++ Rankers/Program.cs
@@ -0,0 +1,19 @@
+﻿//Welcome message
+Console.WriteLine("Rankers");
+Console.WriteLine("-------");
+Console.WriteLine();
+
+//Prepare values
+string data = "Juan,Pedro,Maria,Wapali";
+List<string> rankers = data.Split(',').ToList();
+
+//Show values
+foreach (string ranker in rankers)
+{
+    Console.WriteLine(ranker);
+}
+
+//End
+Console.WriteLine();
+Console.WriteLine("End of program.");
+Console.ReadLine();
